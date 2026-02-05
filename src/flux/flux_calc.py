5bb1655680fd3add1dab68b3f0e10864e8bc9492
Andrea Tanzer <andrea.tanzer@gmx.at>
2026-02-05 14:54:26 +0100

    dumping a testfile for tests

--- src/flux/flux_calc.py
+++ src/flux/flux_calc.py
@@ -1,18 +1,21 @@
 import logging
 import numpy as np
 import astropy.units as u
+from pathlib import Path
 from loaders.run_setup import get_repo_root
 from domain.star import Star
 from astropy.constants import c
 C_LIGHT_A_S = c.to(u.AA / u.s).value # light speed in Angstrom / second
+c = 3e18
 
 
-
-def calculateFluxOnEarth(star: Star):
+def calculateFluxOnEarth(star: Star, output_dir):
     print("Starting to calculate Flux on Earth")
     model_data = load_model_for_temperature(star.effective_temperature)
     print(np.shape(model_data))
     luminosity_lambda = convertIntensityToLuminosity(model_data, star.radius)
+    dump_spectrum_txt(luminosity_lambda, output_dir, filename="flux_Wasp189b.txt", header="wavelength_A  flux  flux_err")
+
 
 def load_model_for_temperature(t_star):
     """
@@ -56,7 +59,6 @@ def load_model_for_temperature(t_star):
     )
 
 def convertIntensityToLuminosity(model_data, r_star):
-
     '''
     Legacy model flux:
     frequency-based stellar model quantity, converted to per-wavelength
@@ -66,11 +68,11 @@ def convertIntensityToLuminosity(model_data, r_star):
     '''
     intensity_lambda        = np.zeros(np.shape(model_data))
     luminosity_lambda  = np.zeros(np.shape(model_data))
-
+    print("c: ", c)
     # we convert from frq to wavelength using lambda in Angstrom: F_lambda = F_nu * c / lambda^2
     # Unit before: erg/cm2/s/Hz, After:  ergs/cm2/s/A
-    intensity_lambda[:,1] = (C_LIGHT_A_S * model_data[:,1])/(model_data[:,0]**2)    
-    intensity_lambda[:,2] = (C_LIGHT_A_S * model_data[:,2])/(model_data[:,0]**2)
+    intensity_lambda[:,1] = (c * model_data[:,1])/(model_data[:,0]**2)    
+    intensity_lambda[:,2] = (c * model_data[:,2])/(model_data[:,0]**2)
 
     # Integrate over stellar surface area (4*pi*R^2) and over solid angle (4*pi)
     # multiply with surface area of star -> ergs/cm2/s/A to ergs/s/A
@@ -80,3 +82,16 @@ def convertIntensityToLuminosity(model_data, r_star):
     luminosity_lambda[:,2]  = intensity_lambda[:,2] * 4 * np.pi * (r_star**2) * 4 * np.pi
 
     return luminosity_lambda
+
+def dump_spectrum_txt(spectrum, output_dir, filename, header):
+    """
+    Writes a 3-column spectrum to output_dir/filename in legacy CUTE-style formatting.
+    """
+    outpath = Path(output_dir) / filename
+
+    np.savetxt(
+        outpath,
+        spectrum,
+        fmt=["%.4f", "%.6E", "%.6E"],
+    )
+
