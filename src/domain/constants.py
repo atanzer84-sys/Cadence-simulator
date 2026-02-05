0055de5dc8a5b32999710740f2fd9b010746459a
Andrea Tanzer <andrea.tanzer@gmx.at>
2026-02-05 14:53:17 +0100

    added constants

---
+++ src/domain/constants.py
@@ -0,0 +1,19 @@
+"""
+Physical and simulator-wide constants.
+Do not modify at runtime. Or ever, because tests will fail.
+"""
+
+# physical constants
+C_LIGHT = 3e18                   # m / s
+H_PLANCK = 6.62607015e-34        # J s
+K_BOLTZMANN = 1.380649e-23       # J / K
+SIGMA_SB = 5.670374419e-8        # W / (m^2 K^4)
+
+# astronomy
+R_SUN = 6.957e10                 # cm
+M_SUN = 1.98847e30               # kg
+L_SUN = 3.828e26                 # W
+PARSEC = 3.0856775814913673e16   # m
+
+# simulator defaults
+DEFAULT_DISTANCE_PC = 10.0
