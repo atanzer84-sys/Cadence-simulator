2a1275c06ec45ef098071619e40eabdfb0c9f852
Andrea Tanzer <andrea.tanzer@gmx.at>
2026-02-16 12:37:12 +0100

    fixed test

--- tests/loaders/test_run_setup.py
+++ tests/loaders/test_run_setup.py
@@ -230,7 +230,10 @@ def global_cfg_log_r(monkeypatch):
         log_r_cool_value=-4.8,
         test_mode=True,
         produce_Plots=False,
-    )
+        n_bias_frames = 0,
+        n_dark_frames = 0,
+        write_dark_and_bias_png = False,
+        write_dark_and_bias_histograms = False,    )
     monkeypatch.setattr(gc, "_GLOBAL", cfg, raising=False)
     return cfg
 
@@ -250,6 +253,10 @@ def global_cfg_log_r_disabled(monkeypatch):
         log_r_cool_value=-4.8,
         test_mode=True,
         produce_Plots=False,
+        n_bias_frames = 0,
+        n_dark_frames = 0,
+        write_dark_and_bias_png = False,
+        write_dark_and_bias_histograms = False,
     )
     monkeypatch.setattr(gc, "_GLOBAL", cfg, raising=False)
     return cfg
