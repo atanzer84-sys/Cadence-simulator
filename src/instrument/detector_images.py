ed2f66d46bbaaffcd707df6b9e2bbdc37167fe8a
Andrea Tanzer <andrea.tanzer@gmx.at>
2026-02-16 15:08:22 +0100

    added dark frames

--- src/instrument/detector_images.py
+++ src/instrument/detector_images.py
@@ -10,7 +10,7 @@ def generate_bias_dark_frames(nuv_cfg, vis_cfg, user_cfg, output_dir):
 
     if n_frames <= 0:
         logging.info("BIAS and DARK: n_bias_frames=%d → no bias frames generated.", n_frames)
-        return [], []
+        return [], [], [], []
 
     logging.info("Creating Bias and Dark Frames.")
     print("Creating Bias and Dark Frames.")
@@ -24,26 +24,28 @@ def generate_bias_dark_frames(nuv_cfg, vis_cfg, user_cfg, output_dir):
     if global_cfg.write_dark_and_bias_png:
         write_frames_png(bias_nuv_frames, "bias", nuv_cfg.channel_name, output_dir)
         write_frames_png(bias_vis_frames, "bias", vis_cfg.channel_name, output_dir)
+        write_frames_png(dark_nuv_frames, "dark", nuv_cfg.channel_name, output_dir)
+        write_frames_png(dark_vis_frames, "dark", vis_cfg.channel_name, output_dir)        
 
 
-    return bias_nuv_frames, bias_vis_frames
+    return bias_nuv_frames, bias_vis_frames, dark_nuv_frames, dark_vis_frames
 
 
 def generate_bias_frames(channel_cfg, n_frames):
 
     logging.info("BIAS: generating %d bias frames for %s (%d x %d).", n_frames, channel_cfg.channel_name, channel_cfg.x_pixels, channel_cfg.y_pixels)
 
-    frames = []
+    bias_frames = []
     for i in range(n_frames):
         frame = generate_bias_frame(channel_cfg)
-        frames.append(frame)
+        bias_frames.append(frame)
         logging.info("BIAS STATS %s frame=%d mean=%g std=%g min=%g max=%g", channel_cfg.channel_name, i, frame.mean(), frame.std(), frame.min(), frame.max())
 
-    return frames
+    return bias_frames
 
 def generate_bias_frame(channel_cfg):
     '''
-    Bias = Offset + Gaussian Noise
+    Bias = Offset (bias_offset) + Gaussian Noise (read_noise)
     '''
     nx = channel_cfg.x_pixels
     ny = channel_cfg.y_pixels
@@ -54,6 +56,18 @@ def generate_bias_frame(channel_cfg):
 
     return bias
 
+def generate_dark_frames(channel_cfg, n_frames, exptime_s):
+
+    logging.info("DARK: generating %d dark frames for %s (%d x %d), exptime_s=%g.", n_frames, channel_cfg.channel_name, channel_cfg.x_pixels, channel_cfg.y_pixels, exptime_s)
+
+    dark_frames = []
+
+    for i in range(n_frames):
+        frame = generate_dark_frame(channel_cfg, exptime_s)
+        dark_frames.append(frame)
+        logging.info("DARK STATS %s frame=%d mean=%g std=%g min=%g max=%g", channel_cfg.channel_name, i, frame.mean(), frame.std(), frame.min(), frame.max())
+
+    return dark_frames
 
 def generate_dark_frame(channel_cfg, exptime_s):
     '''
