0688437941bec6aedb23c33a351472833ded311a
Andrea Tanzer <andrea.tanzer@gmx.at>
2026-02-26 16:56:33 +0100

    added global cfg for cosmic

--- src/instrument/build_science_image.py
+++ src/instrument/build_science_image.py
@@ -4,15 +4,18 @@ from loaders.run_waltzer_context import RunContext
 from configs.channel_config import SpectroscopyChannel
 from instrument.bias_image import generate_bias_image
 from instrument.dark_image import generate_dark_image
+from instrument.cosmic_image import generate_cosmic_rays
+from configs.global_config import GlobalConfig, get_global_config
 
 def build_science_images (spectra_2d_nuv, spectra_2d_vis, nuv: SpectroscopyChannel, vis: SpectroscopyChannel, ctx: RunContext):
 
-    nuv_img = build_science_image(spectra_2d_nuv, nuv, ctx)
-    vis_img = build_science_image(spectra_2d_vis, vis, ctx)
+    cfg = get_global_config()
+    nuv_img = build_science_image(spectra_2d_nuv, nuv, ctx, cfg)
+    vis_img = build_science_image(spectra_2d_vis, vis, ctx, cfg)
     return nuv_img, vis_img
 
 
-def build_science_image(spectra_2d, channel: SpectroscopyChannel, ctx: RunContext):
+def build_science_image(spectra_2d, channel: SpectroscopyChannel, ctx: RunContext, cfg: GlobalConfig):
     logging.info("Science Image generation starting for channel %s", channel.channel_name)
     print(f"Science Image generation starting for channel {channel.channel_name}.")
     nx = channel.x_pixels
@@ -36,6 +39,11 @@ def build_science_image(spectra_2d, channel: SpectroscopyChannel, ctx: RunContex
     photon_noise = apply_photon_noise_gauss_from_spectra2d(spectra_2d*exposure)
     image += photon_noise
     ctx.write_image_png.write_image(image, "PHOTON_NOISE", ctx, channel)
+
+    cosmic = generate_cosmic_rays(ctx, channel, cfg)
+    image += cosmic
+    # ctx.write_image_png.write_image(image, "COSMIC", ctx, channel)
+    
 
     image = image * ccd_gain
     return image
