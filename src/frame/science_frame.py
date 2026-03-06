b4a201304c81d8e3d8a3fbf06d5c124e3188a8e4
Andrea Tanzer <andrea.tanzer@gmx.at>
2026-03-06 17:39:35 +0100

    changed to array of frames

--- src/frame/science_frame.py
+++ src/frame/science_frame.py
@@ -4,16 +4,20 @@ from frame.frame_class import Frame
 from frame.fits_header import append_image_stats_header, append_channel_frame_header, append_base_frame_header
 
 
-def generate_science_frame(image, channel: Channel, base_header):
-    """Generate one science frame. Returns list of 1 frame for consistency with bias/dark pipeline."""
+def generate_science_frame(images, channel: Channel, base_header):
+    """Generate science frame(s). Accepts single image or list of images. Returns list of frames."""
+    if not isinstance(images, list):
+        images = [images]
     exposure_time_s = channel.exposure_s
-    logging.info("SCIENCE: generating 1 science frame for %s (%d x %d), exptime_s=%g.", channel.channel_name, channel.x_pixels, channel.y_pixels, exposure_time_s)
-    print(f"Creating SCIENCE Frames for channel {channel.channel_name}.")
+    logging.info("SCIENCE: generating %d science frame(s) for %s (%d x %d), exptime_s=%g.", len(images), channel.channel_name, channel.x_pixels, channel.y_pixels, exposure_time_s)
+    print(f"Creating SCIENCE Frames for channel {channel.channel_name} ({len(images)} frame(s)).")
 
-    header = append_base_frame_header(base_header, filetype="SCIENCE", channel=channel, index0=0)
-    append_image_stats_header(header, image)
-    append_channel_frame_header(header, channel, exptime_s=exposure_time_s, include_bias=True, include_dark=True)
-
-    frame = Frame(data=image, header=header, frame_type="science", channel_tag=channel.channel_name)
-    return [frame]
+    frames = []
+    for i, image in enumerate(images):
+        header = append_base_frame_header(base_header, filetype="SCIENCE", channel=channel, index0=i)
+        append_image_stats_header(header, image)
+        append_channel_frame_header(header, channel, exptime_s=exposure_time_s, include_bias=True, include_dark=True)
+        frame = Frame(data=image, header=header, frame_type="science", channel_tag=channel.channel_name)
+        frames.append(frame)
+    return frames
 
