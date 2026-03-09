5c3afc66be87dd70241b6af3aa19f16ae6ba42a6
Andrea Tanzer <andrea.tanzer@gmx.at>
2026-03-09 20:31:22 +0100

    changed logs

--- src/frame/bias_frame.py
+++ src/frame/bias_frame.py
@@ -9,6 +9,7 @@ from frame.fits_header import append_image_stats_header, append_channel_frame_he
 def generate_bias_frames(channel: Channel, n_frames, base_header):
 
     logging.info("BIAS: generating %d bias frames for %s (%d x %d).", n_frames, channel.channel_name, channel.x_pixels, channel.y_pixels)
+    
     print(f"Creating BIAS Frames for channel {channel.channel_name}.")
 
     bias_frames = []
@@ -28,10 +29,10 @@ def generate_bias_frame(channel: Channel, header=None):
 
     bias = generate_bias_image(channel) * ccd_gain
 
-    logging.info("BIAS STATS %s mean=%g std=%g min=%g max=%g", channel.channel_name, bias.mean(), bias.std(), bias.min(), bias.max())
 
     if header is not None:
         append_image_stats_header(header, bias)
         append_channel_frame_header(header, channel, exptime_s=0.0, include_bias=True, include_dark=False)
 
+    logging.info("BIAS STATS %s mean=%g std=%g min=%g max=%g", channel.channel_name, bias.mean(), bias.std(), bias.min(), bias.max())
     return Frame(data=bias, header=header, frame_type="bias", channel_tag=channel.channel_name)
--- src/frame/write_fits.py
+++ src/frame/write_fits.py
@@ -9,14 +9,11 @@ def write_fits_frames(frames, headers, frame_type, channel_tag, ctx: RunContext)
         logging.info("Write FITS: no frames for %s channel %s", frame_type, channel_tag)
         return
 
-    logging.info("Writing %d %s frame(s) for channel %s to %s", n_frames, frame_type, channel_tag, ctx.output_dir)
-
     star_name = str(ctx.target_name).replace(" ", "_")
     for k, (frame, header) in enumerate(zip(frames, headers)):
         fname = f"WALTzER_{star_name}_{channel_tag}_{frame_type}_{k:05d}.fits"
         filename = ctx.output_dir / fname
         header.append(("FILENAME", fname, "Output FITS filename"))
         fits.PrimaryHDU(data=frame, header=header).writeto(filename, overwrite=True)
-        logging.debug("Wrote %s", filename)
 
-    logging.info("Finished writing %d FITS file(s)", n_frames)
+    logging.info("WRITE FITS SUMMARY type=%s channel=%s frames=%d outdir=%s", frame_type, channel_tag, n_frames, ctx.output_dir)
