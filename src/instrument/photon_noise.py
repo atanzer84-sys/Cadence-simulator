aac1417447bad62cfada7dfcf3fc0799ad599921
Andrea Tanzer <andrea.tanzer@gmx.at>
2026-04-11 18:12:50 +0200

    refactored name for photon noise

--- src/instrument/photon_noise.py
+++ src/instrument/photon_noise.py
@@ -2,8 +2,8 @@ import numpy as np
 
 _rng = np.random.default_rng()
 
-def generate_photon_noise_from_spectra2d(spectra_2d_exposure):
-    distr = _rng.normal(loc=0.0, scale=1.0, size=spectra_2d_exposure.shape).astype(np.float32)
-    sigma = np.sqrt(np.clip(spectra_2d_exposure, 0, None))
+def generate_photon_noise_from_spectra2d(stellar_component_2d):
+    distr = _rng.normal(loc=0.0, scale=1.0, size=stellar_component_2d.shape).astype(np.float32)
+    sigma = np.sqrt(np.clip(stellar_component_2d, 0, None))
     noise = distr * sigma
     return noise
